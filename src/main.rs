ecd68a86e3dc8bea2b36cbc4c8f17fff4a0cdde3
Donal Burns <donal.w.burns@hotmail.com>
2024-12-22 15:10:33 +0000

    Add uri fixing for static html files
    Its a very hacky solutions right now, so I need to come back to it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use axum::http::uri::PathAndQuery;
+use axum::http::{Error, Request, StatusCode, Uri};
+use axum::response::IntoResponse;
 use axum::routing::post;
 use axum::{Router, routing::get};
 use tokio;
 use tokio::io::AsyncWriteExt;
 
 use std::net::SocketAddr;
+use std::str::FromStr;
+use tower::ServiceExt;
 use tower_http::services::ServeDir;
 use tower_http::trace::{self, TraceLayer};
 use tracing::Level;
@@ -19,7 +24,7 @@ async fn main() {
     // web server configuration
     let app = Router::new()
         // Serve the frontend files so they can be accessed as "/..." in the JS/HTML
-        .nest_service("/", ServeDir::new("frontend/dist"))
+        .nest_service("/", get(static_file_uri_handler))
         // Backend API
         .route("/server", get(hello_world))
         .route("/upload", post(upload_file))
@@ -77,5 +82,69 @@ async fn upload_file(mut file: axum::extract::Multipart) -> axum::response::Redi
     }
 
     // Redirect to a success page once upload is done
-    axum::response::Redirect::to("/upload_success.html")
+    axum::response::Redirect::to("/upload_success")
+}
+
+/// When using Tower's serveDir, it will serve the files as if they were in the root directory
+/// This includes the file extension, meaning that if I want to redirect to another page
+/// on the frontend I need to include the file extension in the redirect URL
+/// and that the file extension will be visible in the URL for users
+/// This is not ideal so in the case of a url not found instead of returning a 404
+/// first try to find the file minus the file extension
+/// This solution greatly inspired by https://github.com/tokio-rs/axum/discussions/446
+async fn static_file_uri_handler(uri: Uri) -> Result<impl IntoResponse, (StatusCode, String)> {
+    match get_static_file(uri.clone()).await {
+        Err(_) => {}
+        Ok(r) => {
+            if r.into_response().status() != StatusCode::NOT_FOUND {
+                // TODO: Need less crappy solution to this, just fighting the borrow checker
+                return Ok(get_static_file(uri).await.unwrap());
+            }
+        }
+    }
+    let mut uri_parts = uri.clone().into_parts();
+    let path_and_query = uri_parts.path_and_query;
+    let (uri_path, uri_params) = match &path_and_query {
+        Some(path_and_query) => (path_and_query.path(), path_and_query.query()),
+        None => {
+            tracing::error!("Failed to get path and query from uri: {:?}", uri);
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to get path and query".to_string(),
+            ));
+        }
+    };
+
+    uri_parts.path_and_query = Some(
+        PathAndQuery::from_str(
+            // only add params if present
+            format!("{}{}", format!("{}.html", uri_path), match uri_params {
+                Some(p) => format!("?{}", p),
+                None => "".to_string(),
+            })
+            .as_str(),
+        )
+        .unwrap(),
+    );
+    let new_uri = Uri::from_parts(uri_parts).unwrap();
+
+    tracing::info!(
+        "uri identified as potential static file, converted uri: {}",
+        new_uri
+    );
+
+    // check if the uri path corresponds to a static file
+    // TODO: should consider caching here so checkups don't need to be done on all requests
+    match get_static_file(new_uri).await {
+        Ok(file) => Ok(file),
+        Err(_) => {
+            tracing::error!("Failed to find static file for uri: {}", uri);
+            Err((StatusCode::NOT_FOUND, "Page not found".to_string()))
+        }
+    }
+}
+
+async fn get_static_file(uri: Uri) -> Result<impl IntoResponse, Error> {
+    let req = Request::builder().uri(uri).body(())?;
+    Ok(ServeDir::new("frontend/dist").oneshot(req).await)
 }
